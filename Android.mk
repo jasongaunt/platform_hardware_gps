c533947bf16c285a03cf6f957974e9a5d628a292
Chih-Wei Huang <cwhuang@linux.org.tw>
2015-04-07 16:21:56 +0800

    enable multiarch builds
    Convert LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH
    to support 64-bit targets.

--- Android.mk
+++ Android.mk
@@ -6,18 +6,18 @@ LOCAL_PATH := $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := gps.default
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := gps.c
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := gps.huawei
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := gps_huawei.c
 include $(BUILD_SHARED_LIBRARY)
 endif
